0f358c7233982ba2d43efbcc1cf15f7eddf03765
Monish-Natarajan <85455908+Monish-Natarajan@users.noreply.github.com>
2021-12-02 16:24:50 +0530

    coco training

---
+++ data/coco_seg.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from pycocotools.coco import COCO
+import skimage.io as io
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import io, transforms
+import torchvision.transforms.functional as TF
+from tqdm.auto import tqdm
+from pathlib import Path
+import random
+from typing import Any, Callable, List, Optional, Tuple
+
+class COCO_Segmentation(Dataset):
+    def __init__(
+        self, 
+        annotations: COCO, 
+        img_ids: List[int], 
+        cat_ids: List[int], 
+        root_path: Path, 
+        transform: Optional[Callable]=None
+    ) -> None:
+        super().__init__()
+        self.annotations = annotations
+        self.img_data = annotations.loadImgs(img_ids)
+        self.cat_ids = cat_ids
+        self.files = [os.path.join(root_path,img["file_name"]) for img in self.img_data]
+        self.transform = transform
+        
+    def __len__(self) -> int:
+        return len(self.files)
+    
+    def __getitem__(self, i: int) -> Tuple[torch.Tensor, torch.LongTensor]:
+        ann_ids = self.annotations.getAnnIds(
+            imgIds=self.img_data[i]['id'], 
+            catIds=self.cat_ids, 
+            iscrowd=None
+        )
+        anns = self.annotations.loadAnns(ann_ids)
+        mask = torch.LongTensor(np.max(np.stack([self.annotations.annToMask(ann) * ann["category_id"] 
+                                                 for ann in anns]), axis=0)).unsqueeze(0)
+        
+        img = io.read_image(self.files[i])
+        if img.shape[0] == 1:
+            img = torch.cat([img]*3)
+        
+        if self.transform is not None:
+            return self.transform(img, mask)
+        
+        return img, mask
